b9b7543b33676b0572cc09699e827aacfea07561
GregCMills <gregscommunication@gmail.com>
2026-04-05 14:00:26 +1000

    update logos

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,14 @@
 import { SignIn } from '@clerk/clerk-react';
+import { dark } from "@clerk/themes";
 
 const Login = () => (
   <div className="flex justify-center mt-20">
-    <SignIn />
+    <SignIn 
+      appearance={{
+        layout: { unsafe_disableDevelopmentModeWarnings: true },
+        baseTheme: dark
+      }} 
+    />
   </div>
 );
 
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,14 @@
 import { SignUp } from '@clerk/clerk-react';
+import { dark } from "@clerk/themes";
 
 const Register = () => (
   <div className="flex justify-center mt-20">
-    <SignUp />
+    <SignUp 
+      appearance={{
+        layout: { unsafe_disableDevelopmentModeWarnings: true },
+        baseTheme: dark
+      }} 
+    />
   </div>
 );
 
