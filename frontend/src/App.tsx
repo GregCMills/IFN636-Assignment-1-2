4f37e55a5bb3a2ec2d1815f7bc8f0f75b2de53ab
GregCMills <gregscommunication@gmail.com>
2026-04-05 15:46:28 +1000

    add route for the dashboard, and default website to dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
-import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/tasks" replace />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/login/*" element={<Login />} />
-        <Route path="/register/*" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/tasks" element={<Tasks />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
